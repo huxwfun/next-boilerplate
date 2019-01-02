90648291b7f8a932e5521fb2acfa5afd9d0eae84
胡小为 <huxiaowei@shimo.im>
2019-01-02 18:05:47 +0800

    headers in pages

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import { useState } from 'react'
+console.log(useState)
+export default class Index extends React.Component {
+  render () {
+    return (
+      <div>
+        <Head>
+          <title>Next boilerplate 🤔</title>
+        </Head>
+        index
+      </div>
+    )
+  }
+}
