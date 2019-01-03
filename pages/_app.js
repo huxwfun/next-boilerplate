004db47f0cb948bc63a20ce7cfa8094c7d3be8de
胡小为 <huxiaowei@shimo.im>
2019-01-03 12:57:56 +0800

    mui

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,10 @@ import App, { Container } from 'next/app'
 import Head from 'next/head'
 import Router from 'next/router'
 import NProgress from 'nprogress'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import JssProvider from 'react-jss/lib/JssProvider'
+import getPageContext from '../config/getPageContext'
 
 Router.events.on('routeChangeStart', url => {
   NProgress.start()
@@ -11,6 +15,19 @@ Router.events.on('routeChangeComplete', () => NProgress.done())
 Router.events.on('routeChangeError', () => NProgress.done())
 
 class MyApp extends App {
+  constructor () {
+    super()
+    this.pageContext = getPageContext()
+  }
+
+  componentDidMount () {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles)
+    }
+  }
+
   render () {
     const { Component, pageProps } = this.props
     return (
@@ -18,7 +35,24 @@ class MyApp extends App {
         <Head>
           <title>My page</title>
         </Head>
-        <Component {...pageProps} />
+        {/* Wrap every page in Jss and Theme providers */}
+        <JssProvider
+          registry={this.pageContext.sheetsRegistry}
+          generateClassName={this.pageContext.generateClassName}
+        >
+          {/* MuiThemeProvider makes the theme available down the React
+              tree thanks to React context. */}
+          <MuiThemeProvider
+            theme={this.pageContext.theme}
+            sheetsManager={this.pageContext.sheetsManager}
+          >
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            {/* Pass pageContext to the _document though the renderPage enhancer
+                to render collected styles on server-side. */}
+            <Component pageContext={this.pageContext} {...pageProps} />
+          </MuiThemeProvider>
+        </JssProvider>
       </Container>
     )
   }
